9f6d9a3ae8d16c59e6265654d2aca29b9523619b
M1guelzinN <antonio.jobs1397@gmail.com>
2022-05-07 12:58:22 -0300

    1.1

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # hora-do-dia
 
 Este é um programa simples que a função de mostrar o horário atual.
-Além de mudar o ambiente dinamicamente de acordo com horário,
-*Você pode mudar o horário também*
+Além de mudar o ambiente dinamicamente de acordo com horário.
+
+(*Você pode mudar o horário também*)
 
 **tecnologias usadas**: HTML/CSS/JS
--- itens/hora.js
+++ itens/hora.js
@@ -7,7 +7,6 @@ let mostrar = window.document.querySelector('div.hora')
     mostrar.innerHTML = `${hora}:${min}`
 
 if(hora >= 5){
-    
 }
 
 function mudar(){
@@ -29,26 +28,28 @@ function mudar(){
     }
     else if(valorDeH <= 5){
         saudac.innerHTML = 'Está de madrugada'
-        bg.style.backgroundImage = "url('../itens/+imagem/madrugada.jpg')"
+        bg.style.backgroundImage = "url('../itens/img/madrugada.jpg')"
 
         colorbg.style.background = 'rgb(19, 32, 54)'
         colorbg1.style.background = 'rgb(19, 32, 54)'
     }
     else if(valorDeH <= 11){
         saudac.innerHTML = 'Bom dia'
-        bg.style.backgroundImage = "url('../itens/imagem/bom-dia.jpg')"
+        bg.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
+        colorbg.style.background = 'rgb(0, 197, 197'
+        colorbg1.style.background = 'rgb(0, 197, 197)'
         
     }
     else if(valorDeH <= 17){
         saudac.innerHTML = 'Boa tarde'
-        bg.style.backgroundImage = "url('../itens/imagem/boa-tarde.jpg')"
+        bg.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
 
         colorbg.style.background = 'rgb(239, 101, 0)'
         colorbg1.style.background = 'rgb(239, 101, 0)'
     }
     else if(valorDeH <= 23){
         saudac.innerHTML = 'Boa noite'
-        bg.style.backgroundImage = "url('../itens/imagem/boa-noite.jpg')"
+        bg.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
 
         colorbg.style.background = 'rgb(59, 62, 104)'
         colorbg1.style.background = 'rgb(59, 62, 104)'
