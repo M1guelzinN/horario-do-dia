5e2b3c329cf86e2beb1e8cc00e36c91044795ff2
M1guelzinN <antonio.jobs1397@gmail.com>
2022-05-07 13:37:19 -0300

    1.4..

--- itens/hora.js
+++ itens/hora.js
@@ -6,24 +6,50 @@ let min = MIN.getMinutes()
 let mostrar = window.document.querySelector('div.hora')
     mostrar.innerHTML = `${hora}:${min}`
 
-    // horário do let.getHours
-if(hora <= 5){
-
-}
-
-
-    // horário  do input 
-function mudar(){
-    let puxad = document.querySelector('#inph')
-    let puxad1 = document.querySelector('#inpm')
-    let valorDeH = Number(puxad.value)
-    let valorDeM = Number(puxad1.value)
-
+    
     let saudac = document.querySelector('.cortesia')
     let bg = document.querySelector('.bg-img')
     
     let colorbg = document.querySelector('.forma-up')
     let colorbg1 = document.querySelector('.forma-down')
+    
+    // horário do let.getHours
+if(hora <= 5){
+    saudac.innerHTML = 'Está de madrugada'
+    bg.style.backgroundImage = "url('itens/img/madrugada.jpg')"
+
+    colorbg.style.background = 'rgb(19, 32, 54)'
+    colorbg1.style.background = 'rgb(19, 32, 54)'
+} 
+else if(hora <= 11){
+    saudac.innerHTML = 'Bom dia'
+    bg.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
+    
+    colorbg.style.background = 'rgb(0, 197, 197'
+    colorbg1.style.background = 'rgb(0, 197, 197)'
+} 
+else if(hora <= 17){
+    saudac.innerHTML = 'Boa tarde'
+    bg.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
+
+    colorbg.style.background = 'rgb(239, 101, 0)'
+    colorbg1.style.background = 'rgb(239, 101, 0)'
+} 
+else if(hora <= 23){
+    saudac.innerHTML = 'Boa noite'
+    bg.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
+
+    colorbg.style.background = 'rgb(59, 62, 104)'
+    colorbg1.style.background = 'rgb(59, 62, 104)'
+}
+
+// horário  do input 
+function mudar(){
+    
+    let puxad = document.querySelector('#inph')
+    let puxad1 = document.querySelector('#inpm')
+    let valorDeH = Number(puxad.value)
+    let valorDeM = Number(puxad1.value)
 
     mostrar.innerHTML = `${valorDeH}:${valorDeM}`
     
