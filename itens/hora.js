17c81b04a1b841999db0dc4dffd3e138b7f66b34
M1guelzinN <antonio.jobs1397@gmail.com>
2022-05-10 12:48:55 -0300

    ok
    organizei o código

--- itens/hora.js
+++ itens/hora.js
@@ -3,87 +3,90 @@ let hora = agora.getHours()
 let MIN = new Date()
 let min = MIN.getMinutes()
 
-let mostrar = window.document.querySelector('div.hora')
-    mostrar.innerHTML = `${hora}:${min}`
+let relogio = window.document.querySelector('.hora')
+    relogio.innerHTML = `${hora}:${min}`
+ 
+let comp = document.querySelector('.comprimentar')
+let Img = document.querySelector('.bg-img')
+    
+let formUp = document.querySelector('.forma-up')     // forma de cima 
+let formDown = document.querySelector('.forma-down')  // forma embaixo
 
-    
-    let saudac = document.querySelector('.cortesia')
-    let bg = document.querySelector('.bg-img')
-    
-    let colorbg = document.querySelector('.forma-up')
-    let colorbg1 = document.querySelector('.forma-down')
-    
-    // horário do let.getHours
+
+    // horário do .getHours
 if(hora <= 5){
-    saudac.innerHTML = 'Está de madrugada'
-    bg.style.backgroundImage = "url('itens/img/madrugada.jpg')"
+    comp.innerHTML = 'Está de madrugada'
+    Img.style.backgroundImage = "url('itens/img/madrugada.jpg')"
 
-    colorbg.style.background = 'rgb(19, 32, 54)'
-    colorbg1.style.background = 'rgb(19, 32, 54)'
+    formUp.style.background = 'rgb(19, 32, 54)'
+    formDown.style.background = 'rgb(19, 32, 54)'
 } 
 else if(hora <= 11){
-    saudac.innerHTML = 'Bom dia'
-    bg.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
+    comp.innerHTML = 'Bom dia'
+    Img.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
     
-    colorbg.style.background = 'rgb(0, 197, 197'
-    colorbg1.style.background = 'rgb(0, 197, 197)'
+    formUp.style.background = 'rgb(0, 197, 197'
+    formDown.style.background = 'rgb(0, 197, 197)'
 } 
 else if(hora <= 17){
-    saudac.innerHTML = 'Boa tarde'
-    bg.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
+    comp.innerHTML = 'Boa tarde'
+    Img.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
 
-    colorbg.style.background = 'rgb(239, 101, 0)'
-    colorbg1.style.background = 'rgb(239, 101, 0)'
+    formUp.style.background = 'rgb(239, 101, 0)'
+    formDown.style.background = 'rgb(239, 101, 0)'
 } 
 else if(hora <= 23){
-    saudac.innerHTML = 'Boa noite'
-    bg.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
+    comp.innerHTML = 'Boa noite'
+    Img.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
 
-    colorbg.style.background = 'rgb(59, 62, 104)'
-    colorbg1.style.background = 'rgb(59, 62, 104)'
+    formUp.style.background = 'rgb(59, 62, 104)'
+    formDown.style.background = 'rgb(59, 62, 104)'
 }
 
-// horário  do input 
-function mudar(){
+// Valor digitado  pelo usuário
+function horaDigitada(){
+    let inputHora = document.querySelector('#inph')
+    let inputMin = document.querySelector('#inpm')
+
+    let valorDeHora = Number(inputHora.value)
+    let valorDeMin = Number(inputMin.value)
+
+    relogio.innerHTML = `${valorDeHora}:${valorDeMin}`
     
-    let puxad = document.querySelector('#inph')
-    let puxad1 = document.querySelector('#inpm')
-    let valorDeH = Number(puxad.value)
-    let valorDeM = Number(puxad1.value)
-
-    mostrar.innerHTML = `${valorDeH}:${valorDeM}`
-    
-    if(valorDeH < 0 || valorDeH > 23 || valorDeM < 0 || valorDeM > 59){
-        alert('O horário não existe')
-    }
-    else if(valorDeH <= 5){
-        saudac.innerHTML = 'Está de madrugada'
-        bg.style.backgroundImage = "url('itens/img/madrugada.jpg')"
-
-        colorbg.style.background = 'rgb(19, 32, 54)'
-        colorbg1.style.background = 'rgb(19, 32, 54)'
-    }
-    else if(valorDeH <= 11){
-        saudac.innerHTML = 'Bom dia'
-        bg.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
-        colorbg.style.background = 'rgb(0, 197, 197'
-        colorbg1.style.background = 'rgb(0, 197, 197)'
-        
-    }
-    else if(valorDeH <= 17){
-        saudac.innerHTML = 'Boa tarde'
-        bg.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
-
-        colorbg.style.background = 'rgb(239, 101, 0)'
-        colorbg1.style.background = 'rgb(239, 101, 0)'
-    }
-    else if(valorDeH <= 23){
-        saudac.innerHTML = 'Boa noite'
-        bg.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
-
-        colorbg.style.background = 'rgb(59, 62, 104)'
-        colorbg1.style.background = 'rgb(59, 62, 104)'
-    }
-    puxad.value = ''
-    puxad1.value = ''
+if(valorDeHora < 0 || valorDeHora > 23 || valorDeMin < 0 || valorDeMin > 59){
+    relogio.innerHTML = "Invalido"
+    inputHora.value = ''
+    inputMin.value = ''
+        return alert('O horário não existe')
 }
+else if(valorDeHora <= 5){
+    comp.innerHTML = 'Está de madrugada'
+    Img.style.backgroundImage = "url('itens/img/madrugada.jpg')"
+
+    formUp.style.background = 'rgb(19, 32, 54)'
+    formDown.style.background = 'rgb(19, 32, 54)'
+}
+else if(valorDeHora <= 11){
+    comp.innerHTML = 'Bom dia'
+    Img.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
+    formUp.style.background = 'rgb(0, 197, 197'
+    formDown.style.background = 'rgb(0, 197, 197)'
+    
+}
+else if(valorDeHora <= 17){
+    comp.innerHTML = 'Boa tarde'
+    Img.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
+
+    formUp.style.background = 'rgb(239, 101, 0)'
+    formDown.style.background = 'rgb(239, 101, 0)'
+}
+else if(valorDeHora <= 23){
+    comp.innerHTML = 'Boa noite'
+    Img.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
+
+    formUp.style.background = 'rgb(59, 62, 104)'
+    formDown.style.background = 'rgb(59, 62, 104)'
+}
+    inputHora.value = ''
+    inputMin.value = ''
+}// fim da function
