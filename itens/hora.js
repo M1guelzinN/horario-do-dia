24254e70d2778ef0b30e2ae918e4dc11a48af839
M1guelzinN <antonio.jobs1397@gmail.com>
2022-05-25 12:28:12 -0300

    Update hora.js

--- itens/hora.js
+++ itens/hora.js
@@ -1,7 +1,6 @@
-let agora = new Date()
-let hora = agora.getHours()
-let MIN = new Date()
-let min = MIN.getMinutes()
+function getData(){
+let hora = new Date().getHours()
+let min = new Date().getMinutes()
 
 let relogio = window.document.querySelector('.hora')
     relogio.innerHTML = `${hora}:${min}`
@@ -12,81 +11,90 @@ let Img = document.querySelector('.bg-img')
 let formUp = document.querySelector('.forma-up')     // forma de cima 
 let formDown = document.querySelector('.forma-down')  // forma embaixo
 
+    if(hora <= 5){
+        comp.innerHTML = 'Está de madrugada'
+        Img.style.backgroundImage = "url('itens/img/madrugada.jpg')"
 
-    // horário do .getHours
-if(hora <= 5){
-    comp.innerHTML = 'Está de madrugada'
-    Img.style.backgroundImage = "url('itens/img/madrugada.jpg')"
+        formUp.style.background = 'rgb(20, 15, 64)'
+        formDown.style.background = 'rgb(20, 15, 64)'
+    } 
+    else if(hora <= 11){
+        comp.innerHTML = 'Bom dia'
+        Img.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
+        
+        formUp.style.background =   'rgb(0, 157, 63)'
+        formDown.style.background = 'rgb(0, 157, 63)'
+    } 
+    else if(hora <= 17){
+        comp.innerHTML = 'Boa tarde'
+        Img.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
 
-    formUp.style.background = 'rgb(20, 15, 64)'
-    formDown.style.background = 'rgb(20, 15, 64)'
-} 
-else if(hora <= 11){
-    comp.innerHTML = 'Bom dia'
-    Img.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
-    
-    formUp.style.background =   'rgb(0, 157, 63)'
-    formDown.style.background = 'rgb(0, 157, 63)'
-} 
-else if(hora <= 17){
-    comp.innerHTML = 'Boa tarde'
-    Img.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
+        formUp.style.background = 'rgb(210, 86, 11)'
+        formDown.style.background = 'rgb(210, 86, 11)'
+    } 
+    else if(hora <= 23){
+        comp.innerHTML = 'Boa noite'
+        Img.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
 
-    formUp.style.background = 'rgb(210, 86, 11)'
-    formDown.style.background = 'rgb(210, 86, 11)'
-} 
-else if(hora <= 23){
-    comp.innerHTML = 'Boa noite'
-    Img.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
-
-    formUp.style.background = 'rgb(76, 43, 91)'
-    formDown.style.background = 'rgb(76, 43, 91)'
-}
+        formUp.style.background = 'rgb(76, 43, 91)'
+        formDown.style.background = 'rgb(76, 43, 91)'
+    }
+} getData()
 
 // Valor digitado  pelo usuário
 function horaDigitada(){
-    let inputHora = document.querySelector('#inph')
-    let inputMin = document.querySelector('#inpm')
 
-    let valorDeHora = Number(inputHora.value)
-    let valorDeMin = Number(inputMin.value)
+    
+let comp = document.querySelector('.comprimentar')
+let Img = document.querySelector('.bg-img')
 
+let formUp = document.querySelector('.forma-up')    
+let formDown = document.querySelector('.forma-down')
+
+let inputHora = document.querySelector('#inph')
+let inputMin = document.querySelector('#inpm')
+let valorDeHora = Number(inputHora.value)
+let valorDeMin = Number(inputMin.value)
+
+let relogio = window.document.querySelector('.hora')
     relogio.innerHTML = `${valorDeHora}:${valorDeMin}`
     
-if(valorDeHora < 0 || valorDeHora > 23 || valorDeMin < 0 || valorDeMin > 59){
-    relogio.innerHTML = "Invalido"
+    if(valorDeHora < 0 || valorDeHora > 23 || valorDeMin < 0 || valorDeMin > 59){
+        relogio.innerHTML = "Invalido"
+        inputHora.value = ''
+        inputMin.value = ''
+            return alert('O horário não existe')
+    }
+    else if(valorDeHora <= 5){
+        comp.innerHTML = 'Está de madrugada'
+        Img.style.backgroundImage = "url('itens/img/madrugada.jpg')"
+
+        formUp.style.background = 'rgb(20, 15, 64)'
+        formDown.style.background = 'rgb(20, 15, 64)'
+    }
+    else if(valorDeHora <= 11){
+        comp.innerHTML = 'Bom dia'
+        Img.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
+
+        formUp.style.background =   'rgb(0, 157, 63)'
+        formDown.style.background = 'rgb(0, 157, 63)'
+    }
+    else if(valorDeHora <= 17){
+        comp.innerHTML = 'Boa tarde'
+        Img.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
+
+        formUp.style.background = 'rgb(210, 86, 11)'
+        formDown.style.background = 'rgb(210, 86, 11)'
+    }
+    else if(valorDeHora <= 23){
+        comp.innerHTML = 'Boa noite'
+        Img.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
+
+        formUp.style.background = 'rgb(56, 43, 91)'
+        formDown.style.background = 'rgb(56, 43, 91)'
+    }
+
     inputHora.value = ''
     inputMin.value = ''
-        return alert('O horário não existe')
-}
-else if(valorDeHora <= 5){
-    comp.innerHTML = 'Está de madrugada'
-    Img.style.backgroundImage = "url('itens/img/madrugada.jpg')"
+} 
 
-    formUp.style.background = 'rgb(20, 15, 64)'
-    formDown.style.background = 'rgb(20, 15, 64)'
-}
-else if(valorDeHora <= 11){
-    comp.innerHTML = 'Bom dia'
-    Img.style.backgroundImage = "url('itens/img/bom-dia.jpg')"
-
-    formUp.style.background =   'rgb(0, 157, 63)'
-    formDown.style.background = 'rgb(0, 157, 63)'
-}
-else if(valorDeHora <= 17){
-    comp.innerHTML = 'Boa tarde'
-    Img.style.backgroundImage = "url('itens/img/boa-tarde.jpg')"
-
-    formUp.style.background = 'rgb(210, 86, 11)'
-    formDown.style.background = 'rgb(210, 86, 11)'
-}
-else if(valorDeHora <= 23){
-    comp.innerHTML = 'Boa noite'
-    Img.style.backgroundImage = "url('itens/img/boa-noite.jpg')"
-
-    formUp.style.background = 'rgb(56, 43, 91)'
-    formDown.style.background = 'rgb(56, 43, 91)'
-}
-    inputHora.value = ''
-    inputMin.value = ''
-}// fim da function
